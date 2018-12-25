f346f6a7503784087d98bd386ddf6d9ae351e3dd
Yiding He <yiding.he@gmail.com>
2018-12-25 18:43:32 +0800

    实现下载 launcher.jar 并执行远程 app

--- app-repository-server/src/main/resources/templates/start.template.sh
+++ app-repository-server/src/main/resources/templates/start.template.sh
@@ -2,4 +2,4 @@
 echo Hello, this is \'{{appname}}\'!
 
 curl -s {{urlPrefix}}/launcher.jar -o launcher.jar
-java -jar launcher.jar {{urlPrefix}}/apps/hello {{args}}
+java -jar launcher.jar {{urlPrefix}}/apps/{{appname}} {{args}}
